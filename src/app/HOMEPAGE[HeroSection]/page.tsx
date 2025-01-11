7fb412a392a9303653913f9aef0789c34b90aa34
Muhammad Atiq Ur Rehman <146697001+Atiq-Developer@users.noreply.github.com>
2025-01-11 14:16:51 +0500

    Updated contact page and other changes

--- src/app/HOMEPAGE[HeroSection]/page.tsx
+++ src/app/HOMEPAGE[HeroSection]/page.tsx
@@ -103,10 +103,12 @@ export default function HeroSection() {
               ].map((blog, index) => (
                 <div key={index} className="p-4 md:w-1/3">
                   <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg flex flex-col justify-between">
-                    <img
+                    <Image
                       className="lg:h-48 md:h-36 w-full object-cover object-center"
                       src={blog.image}
                       alt={blog.title}
+                      width={400}
+                      height={400}
                     />
                     <div className="p-6 flex-grow">
                       <h3 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-200">
